511ac5cf9e0122e3f2cdc96d17f3fbbad6cc3510
Issac-Kim <kimi6@ri.edu>
2019-05-30 17:22:06 -0400

    start taking reusable components of the page and converting them to partials

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+# imports for flask app
 import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# route to the home page
+# renders a static web page
 @app.route("/")
 def home():
     return render_template("resources/templates/index.html")
+
+# route for projects page
+# opens up projects.json and dynamically populates page with different projects using jinja
+@app.route("/projects")
+def projects():
+    
+    return render_template("resources/templates/project.html")
