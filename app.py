976cc3313b263fc79c939cf18ee1c92b1ac5f290
Issac-Kim <kimi6@ri.edu>
2019-05-30 17:29:47 -0400

    fix issue with rendering main template, fixing issues with routing to stylesheet and js

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 # imports for flask app
 import os
 from flask import Flask, render_template
-app = Flask(__name__)
+
+# create the app
+# change the template folder to adjust to the directory structure being used
+app = Flask(__name__, template_folder='./resources/templates/')
 
 # route to the home page
 # renders a static web page
 @app.route("/")
 def home():
-    return render_template("resources/templates/index.html")
+    return render_template("index.html")
 
 # route for projects page
 # opens up projects.json and dynamically populates page with different projects using jinja
 @app.route("/projects")
 def projects():
-    
-    return render_template("resources/templates/project.html")
+
+    return render_template("project.html")
