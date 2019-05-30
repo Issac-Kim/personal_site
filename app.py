a1b7f4599603a05cb4de3e379557a866e39776f3
Issac-Kim <kimi6@ri.edu>
2019-05-30 17:46:29 -0400

    errors with loading pages at the moment

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-# imports for flask app
 import os
 from flask import Flask, render_template
-
-# create the app
-# change the template folder to adjust to the directory structure being used
-app = Flask(__name__, template_folder='./resources/templates/')
+app = Flask(__name__)
 
 # route to the home page
 # renders a static web page
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "i"
+    #return render_template("index.html")
 
 # route for projects page
 # opens up projects.json and dynamically populates page with different projects using jinja
 @app.route("/projects")
 def projects():
-
-    return render_template("project.html")
+    #return render_template("project.html")
+    return "hi"
